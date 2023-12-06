06e2b0e818ee1b7166ea86ebc22595172eb182fe
Aaron <Aaron9034@Hotmail.com>
2023-12-06 01:01:55 -0800

    rename m1/m2a/m3a and fix ti_sample.py

--- tutorials/ti_sample.py
+++ tutorials/ti_sample.py
@@ -4,26 +4,34 @@
 
 import numpy as np
 import json
+import glob
+from h5pulsar.pulsar import FilePulsar
 
-import numpy as np
 from enterprise_extensions import sampler, models
 
-import pickle
-
-def sample_2a(psrs, noise_params, gwbfreqs=14, Tmax=1e7, num_samples=5e6):
-    pta = models.model_2a(psrs, gamma_common=4.33, noisedict=noise_params, n_gwbfreqs=14, tm_marg=True, tm_svd=True)
-    outdir = './chains/15yr_ti_m2a_gamma_4.33_{0}_{1}'.format(gwbfreqs, Tmax)
+def sample_irn(psrs, noise_params, gwbfreqs=14, Tmax=1e7, num_samples=5e6):
+    pta = models.model_1(psrs, noisedict=noise_params, tm_marg=True, tm_svd=True)
+    outdir = './chains/15yr_ti_irn_{0}_{1}'.format(gwbfreqs, Tmax)
     emp_dist = datadir + 'emp_distr.pkl'
-    sampler1 = sampler.setup_sampler(pta, outdir=outdir, resume=True, human='Aaron', empirical_distr=emp_dist)
+    sampler1 = sampler.setup_sampler(pta, outdir=outdir, resume=True, empirical_distr=emp_dist)
     N = int(num_samples)
     x0 = np.hstack([p.sample() for p in pta.params])
     sampler1.sample(x0, N, SCAMweight=30, AMweight=15, DEweight=50, writeHotChains=True, Tmax=Tmax, hotChain=True)
 
-def sample_3a(psrs, noise_params, gwbfreqs=14, Tmax=1e7, num_samples=5e6):
-    pta = models.model_3a(psrs, noisedict=noise_params, n_gwbfreqs=14, tm_marg=True, tm_svd=True)
-    outdir = './chains/15yr_ti_m3a_{0}_{1}'.format(gwbfreqs, Tmax)
+def sample_curn(psrs, noise_params, gwbfreqs=14, Tmax=1e7, num_samples=5e6):
+    pta = models.model_2a(psrs, noisedict=noise_params, n_gwbfreqs=14, tm_marg=True, tm_svd=True)
+    outdir = './chains/15yr_ti_curn_varied_gamma_{0}_{1}'.format(gwbfreqs, Tmax)
     emp_dist = datadir + 'emp_distr.pkl'
-    sampler1 = sampler.setup_sampler(pta, outdir=outdir, resume=True, human='Aaron', empirical_distr=emp_dist)
+    sampler1 = sampler.setup_sampler(pta, outdir=outdir, resume=True, empirical_distr=emp_dist)
+    N = int(num_samples)
+    x0 = np.hstack([p.sample() for p in pta.params])
+    sampler1.sample(x0, N, SCAMweight=30, AMweight=15, DEweight=50, writeHotChains=True, Tmax=Tmax, hotChain=True)
+
+def sample_hd(psrs, noise_params, gwbfreqs=14, Tmax=1e7, num_samples=5e6):
+    pta = models.model_3a(psrs, noisedict=noise_params, n_gwbfreqs=14, tm_marg=True, tm_svd=True)
+    outdir = './chains/15yr_ti_hd_varied_gamma_{0}_{1}'.format(gwbfreqs, Tmax)
+    emp_dist = datadir + 'emp_distr.pkl'
+    sampler1 = sampler.setup_sampler(pta, outdir=outdir, resume=True, empirical_distr=emp_dist)
     N = int(num_samples)
     x0 = np.hstack([p.sample() for p in pta.params])
     sampler1.sample(x0, N, SCAMweight=30, AMweight=15, DEweight=50, writeHotChains=True, Tmax=Tmax, hotChain=True)
@@ -32,8 +40,11 @@ if __name__ == '__main__':
     datadir = './data/'
     with open(datadir + 'v1p1_wn_dict.json') as f:
         wn_params = json.load(f)
-    pickle_loc = datadir + 'v1p1_de440_pint_bipm2019.pkl'
-    with open(pickle_loc, 'rb') as f:
-        psrs = pickle.load(f)
 
-    sample_2a(psrs, wn_params)
+    psrs = []
+    for hdf5_file in glob.glob(datadir + '/hdf5/*.hdf5'):
+        psrs.append(FilePulsar(hdf5_file))
+    print('Loaded {0} pulsars from hdf5 files'.format(len(psrs)))
+
+    # change this to whichever model you would like to sample:
+    sample_curn(psrs, wn_params)
